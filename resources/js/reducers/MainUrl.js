84a63900d8a33c31bd146cbae269c36b34744e26
Samuel Olubayo <samuelolubayo@gmail.com>
2022-03-11 14:25:26 +0100

    temp fix --1.2

--- resources/js/reducers/MainUrl.js
+++ resources/js/reducers/MainUrl.js
@@ -1,4 +1,4 @@
-const initState = process.env.MIX_ENDPOINT_URL;
+const initState = "http://fuelcreditapp-env.eba-7wrwh2nj.us-east-2.elasticbeanstalk.com/api/";
 const MainUrl = (state=initState,action)=>{
     switch (action.type) {
         case "CHANGE":
